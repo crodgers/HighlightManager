949520ce0755ea82b59a79c30ee193a1047e88c9
Christopher Douglas Rodgers <christopher.douglas.rodgers@gmail.com>
2017-01-10 14:29:07 +0000

    Add basick session management

--- server.js
+++ server.js
@@ -1,5 +1,28 @@
 const app = require('express')();
+const session = require('express-session');
+const dbStore = require('connect-mongo')(session);
+const mongoose = require('mongoose');
 const routes = require('./routes');
+
+//connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI);
+
+const sessionStore = new dbStore(
+  {
+    mongooseConnection: mongoose.connection,
+    collection: 'highlightSessions'
+  });
+
+
+app.use(session({
+  cookie: {
+    maxAge: 1000 * 60 * 60 * 24 * 7 // 1 week
+  },
+  secret: process.env.SESSION_SECRET,
+  store: sessionStore,
+  resave: true,
+  saveUninitialized: true
+}));
 
 //  Connect all our routes to our application
 app.use('/', routes);
