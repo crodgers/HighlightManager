9c5782edb299506a8ac272be15ffe05cdc2ac13c
Christopher Douglas Rodgers <christopher.douglas.rodgers@gmail.com>
2017-02-03 18:43:34 +0000

    fix the tests so that they actually test something.

--- test/test_routes.js
+++ test/test_routes.js
@@ -10,11 +10,37 @@ describe('root route', function () {
     beforeEach(() => {
         server = require('../server');
     });
+
     afterEach((done) => {
         server.close(done);
-    })
-    it('should return 200', (done) => {
-        request(server).get('/').expect(200)
+    });
+
+    it('status code should be 200', (done) => {
+        request(server).get('/')
+            .expect(200)
+            .end((err, res) => {
+                if (err) throw err;
+            });
         done();
     });
+});
+
+describe('highlights route', function () {
+   var server;
+    beforeEach(() => {
+        server = require('../server');
+    });
+
+    afterEach((done) => {
+        server.close(done);
+    });
+   
+   it('should redirect with 302', (done) => {
+        request(server).get('/highlights')
+            .expect(302)
+            .end((err, res) => {
+                if (err) throw err;
+            });
+        done();
+    }) 
 });
